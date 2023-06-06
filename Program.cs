2ae40944e15852a789e3c7bcbb6e14f4e130025e
WhatIsFun <mohammed110051@gmail.com>
2023-06-06 12:57:29 +0400

    Calculator

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace functions_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" _     _  _______  ___      _______  _______  __   __  _______    _______  _______ ");
+            Console.WriteLine("| | _ | ||       ||   |    |       ||       ||  |_|  ||       |  |       ||       | ");
+            Console.WriteLine("| || || ||    ___||   |    |       ||   _   ||       ||    ___|  |_     _||   _   | ");
+            Console.WriteLine("|       ||   |___ |   |    |       ||  | |  ||       ||   |___     |   |  |  | |  | ");
+            Console.WriteLine("|       ||    ___||   |___ |      _||  |_|  ||       ||    ___|    |   |  |  |_|  | ");
+            Console.WriteLine("|   _   ||   |___ |       ||     |_ |       || ||_|| ||   |___     |   |  |       | ");
+            Console.WriteLine("| __||__||_______||_______||_______||_______||_|  |_||_______|     |___|  |_______|");
+            Console.WriteLine(" _     _  __   __  _______  _______  ___   _______  _______  __   __  __    _       ");
+            Console.WriteLine("| | _ | ||  | |  ||   _   ||       ||   | |       ||       ||  | |  ||  |  | |      ");
+            Console.WriteLine("| || || ||  |_|  ||  |_|  ||_     _||   | |  _____||    ___||  | |  ||   |_| |      ");
+            Console.WriteLine("|       ||       ||       |  |   |  |   | | |_____ |   |___ |  |_|  ||       |      ");
+            Console.WriteLine("|       ||       ||       |  |   |  |   | |_____  ||    ___||       ||  _    |      ");         
+            Console.WriteLine("|   _   ||   _   ||   _   |  |   |  |   |  _____| ||   |    |       || | |   |      ");
+            Console.WriteLine("|__| |__||__| |__||__| |__|  |___|  |___| |_______||___|    |_______||_|  |__|      ");
+            Console.WriteLine(" _______  _______  ___      _______  __   __  ___      _______  _______  _______  ______");
+            Console.WriteLine("|       ||   _   ||   |    |       ||  | |  ||   |    |   _   ||       ||       ||    _ |");
+            Console.WriteLine("|       ||  |_|  ||   |    |       ||  | |  ||   |    |  |_|  ||_     _||   _   ||   | ||");
+            Console.WriteLine("|       ||       ||   |    |       ||  |_|  ||   |    |       |  |   |  |  | |  ||   |_||_");
+            Console.WriteLine("|      _||       ||   |___ |      _||       ||   |___ |       |  |   |  |  |_|  ||    __  |");
+            Console.WriteLine("|     |_ |   _   ||       ||     |_ |       ||       ||   _   |  |   |  |       ||   |  | |");
+            Console.WriteLine("|_______||__| |__||_______||_______||_______||_______||__| |__|  |___|  |_______||___|  |_|");
+            
+
+            printMenu();
+        }
+
+        static void printMenu() {
+
+            Console.WriteLine("A) Add");
+            Console.WriteLine("B) ");
+            Console.WriteLine("C) ");
+            Console.WriteLine("D) ");
+            
+
+            /*switch (task) { 
+                case "A":
+                    Console.WriteLine();
+
+                case "B":
+                    Console.WriteLine();
+
+                case "C":
+                    Console.WriteLine();
+
+                case "D":
+                    Console.WriteLine();
+
+                case "#":
+
+
+
+
+
+
+            }*/
+
+
+        }
+    }
+}
